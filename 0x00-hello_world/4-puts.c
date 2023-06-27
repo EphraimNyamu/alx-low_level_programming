51bc25997e3b903acc121b7731e6175a87604d83
EphraimNyamu <epraimnyamu@gmail.com>
2023-06-27 07:06:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- a program to print a message with puts function
+ * Return: value should be 0 for successful compilation
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
