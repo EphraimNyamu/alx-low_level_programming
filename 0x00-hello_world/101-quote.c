f387d676e7208fee8934e595af25ead8e52e86b7
EphraimNyamu <epraimnyamu@gmail.com>
2023-06-27 09:59:14 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - C program to print a line with any function to the standard error
+ * Return: successful compilation returns 1
+ */
+int main(void)
+{
+	fput("and that piece ofand that piece of art is useful\"- Dora Korpar, 2015-10-19\n",stdout);
+	return (1);
+}
