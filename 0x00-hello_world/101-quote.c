2d6818945c919a611eff0335b6f801783f251b92
EphraimNyamu <epraimnyamu@gmail.com>
2023-06-27 10:29:43 -0700

    work pls

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistdio.h>
 /**
  * main - C program to print a line with any function to the standard error
  * Return: successful compilation returns 1
  */
 int main(void)
 {
-fputs("and that piece ofand that piece of art is useful\"-
-Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fputs("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19\n", stdout);
+return (1);
 }
