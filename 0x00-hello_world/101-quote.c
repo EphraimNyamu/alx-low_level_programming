f79e8f8d95eb360e76097ff84c8a2375a53d075c
EphraimNyamu <epraimnyamu@gmail.com>
2023-06-27 10:10:58 -0700

    check

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-fput("and that piece ofand that piece of art is useful\"-
+fputs("and that piece ofand that piece of art is useful\"-
 Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
