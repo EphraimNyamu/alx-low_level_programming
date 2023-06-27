418a8ff009eef7224d041399c62a4b918821eca2
EphraimNyamu <epraimnyamu@gmail.com>
2023-06-27 07:34:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - c program to print proper grammar
+ * followed by a new line using printf function
+ * Return: successful compilation return value is 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
