12824e04ec0f03d268ae16912b4b5cb6f134abba
Ephraim@Nyamu <epraimnyamu@gmail.com>
2023-06-29 20:46:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - C program to print a line with any function to the standard error
@@ -6,7 +5,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\"-Dora Korpar,
-2015-10-19\n", stdout);
-return (1);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
 }
