18e3e1ce8a32e6fa0d1349f6efa7311df9aebb5e
Ephraim@Nyamu <epraimnyamu@gmail.com>
2023-06-30 21:34:51 -0700

    go through

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * main -prints alphabets in uppercase followed by lowercase
- * Return: 0 for successful compilation
+ * main - prints uppercase alphabets followed by lowcase
+ * Return: 0 on successful compilation
  */
-void main(void)
+int main(void)
 {
 int ch;
 clrscr();
-/* print uppercase letters */
-for (ch = ‘A’; ch <= ‘Z’; ch++)
+for (ch = 'A'; ch <= 'Z'; ch++)
 putchar(ch);
-putchar(‘\n');
-/* print lowercase letters */
-for(ch = ‘a’; ch <= ‘z’; ch++)
+putchar('\n');
+for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-putchar(‘\n’);
+putchar('\n');
 getch();
 }
+
+
