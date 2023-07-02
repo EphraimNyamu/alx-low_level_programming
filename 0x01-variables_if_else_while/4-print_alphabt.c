b8a459369c067b031bd5fd0d43df0b9fb230c947
Ephraim@Nyamu <epraimnyamu@gmail.com>
2023-07-02 16:27:48 -0700

    adg

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * main - prints the alphabet without q and e.
+ * main- Prints the alphabet without q and e.
  * Returns: 0 (success)
  */
 int main(void)
