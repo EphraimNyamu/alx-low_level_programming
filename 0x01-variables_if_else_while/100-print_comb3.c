f59c2dcb178be1744f65581d0233503eaf912afa
Ephraim@Nyamu <epraimnyamu@gmail.com>
2023-07-02 20:38:56 -0700

    ahsgs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,19 @@ for (a = 48; a <= 56; a++)
 {
 for (b = 49; b <= 57; b++)
 {
-if (a != 56 && b > 57)
+if (b > a)
 {
 putchar(a);
 putchar(b);
+if (a != 56 || b != 57)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
 putchar('\n');
 return (0);
 }
+
