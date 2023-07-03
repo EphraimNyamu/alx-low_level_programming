8b31633bdb6a2bd3bb0c85a96e2aead2dc2012b7
Ephraim@Nyamu <epraimnyamu@gmail.com>
2023-07-02 18:36:26 -0700

    absh

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of numbers
+* with no two numbers same in pairing
+* Return: 0 means success
+*/
+int main(void)
+{
+int a, b;
+for (a = 48; a <= 57; a++)
+{
+for (b = 49; b <= 57; b++)
+{
+if (a != b && b > a)
+{
+putchar(a);
+putchar(b);
+putchar(',');
+}
+}
+}
+putchar('\n');
+return (0);
+}
