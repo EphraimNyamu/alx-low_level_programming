b2730089da8fda0bab5e5aff0332ee9e6aed58e2
Ephraim@Nyamu <epraimnyamu@gmail.com>
2023-07-02 18:51:35 -0700

    adfg

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,16 +15,14 @@ for (j = 49; j < 58; j++)
 for (k = 50; k < 58; k++)
 {
 if (k > j && j > i)
-}
 putchar(i);
 putchar(j);
 putchar(k);
+}
 if (i != 55 || j != 56)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 }
