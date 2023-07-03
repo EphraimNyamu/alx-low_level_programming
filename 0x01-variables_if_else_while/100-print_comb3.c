5dda74611c1f6ce66e5901d0537115666fa86b04
Ephraim@Nyamu <epraimnyamu@gmail.com>
2023-07-02 19:38:48 -0700

    ahsgs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,24 @@
 */
 int main(void)
 {
-int a, b;
+int a, b, c;
 for (a = 48; a <= 57; a++)
 {
 for (b = 49; b <= 57; b++)
 {
-if (a != b && b > a)
+for (c = 50; c < 58; c++)
+{
+if(c > b && b > a)
 {
 putchar(a);
 putchar(b);
+putchar(c);
+if (a != 55 || b != 56)
+{
 putchar(',');
+putchar(' ');
+}
+}
 }
 }
 }
