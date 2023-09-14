27f728f74d269557b5994c0427e2399ac845f22e
EphraimNyamu <epraimnyamu@gmail.com>
2023-09-13 21:50:00 -0700

    wwjwhgsa

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,10 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
 /**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
- */
+* struct op - A struct op.
+* @op: The operator.
+* @f: The associated function.
+*/
 typedef struct op
 {
 char *op;
@@ -17,4 +17,3 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 #endif
-
